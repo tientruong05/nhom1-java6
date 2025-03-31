675cfaee1541312e6af97ee6d6bfe9cac1b9cfd2
nhinijja <nhi81924@gmail.com>
2025-03-31 19:01:36 +0700

    Update ProductDTO.java

--- ASM_VIP/src/main/java/poly/edu/DTO/ProductDTO.java
+++ ASM_VIP/src/main/java/poly/edu/DTO/ProductDTO.java
@@ -1,18 +1,23 @@
 package poly.edu.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import poly.edu.entity.SubCategoryEntity;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
     private int id;
     private String name;
     private String image;
     private float price;
-    private Float discountedPrice; // Giá sau giảm giá
-    private Float discountPercentage; // Phần trăm giảm giá
-    private int qty; // Số lượng
-    private String description; // Mô tả
-    private int status; // Trạng thái
-    private SubCategoryEntity subCategory; // Để hiển thị Loại hàng và Hãng
+    private float discountedPrice;
+    private Float discountPercentage;
+    private int qty;
+    private int sellProgress; // % tiến trình đã bán
+    private int status; // 0: không hoạt động, 1: hoạt động
+    private String description;
+    private SubCategoryEntity subCategory;
 }
